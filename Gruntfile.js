17e42443c5703001fc0e3cda6225d90e1a1fc9c0
MrMartineau <zander@martineau.tv>
2013-08-16 14:38:10 +0100

    Update Grunt and dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,6 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-devtools');
 
 	// =============
 	// === Tasks ===
